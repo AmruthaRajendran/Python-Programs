a4d5cde41467f576c13110fe029084bcfc9716db
Amrutha R <65005169+AmruthaRajendran@users.noreply.github.com>
2020-06-30 18:23:04 +0530

    Update Array_Manipulation.py

--- Array_Manipulation.py
+++ Array_Manipulation.py
@@ -1,4 +1,4 @@
-HackerRank problem
+ # HackerRank problem
 
 ''' Question:Starting with a 1-indexed array of zeros and a list of operations, for each operation add a value to each of the array element between two given indices, inclusive. 
 Once all operations have been performed, return the maximum value in your array.
