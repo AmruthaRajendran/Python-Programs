685a04dc93f3624e16277c7c45e0c47111635ab2
Amrutha R <65005169+AmruthaRajendran@users.noreply.github.com>
2020-09-09 20:21:38 +0530

    Update Encryption.py

--- Encryption.py
+++ Encryption.py
@@ -1,6 +1,26 @@
 # HackerRank Problem
 
-[Question](https://www.hackerrank.com/challenges/encryption/problem)
+''' Question:(https://www.hackerrank.com/challenges/encryption/problem)
+Sample Input 0
+haveaniceday
+Sample Output 0
+hae and via ecy
+Explanation 0
+L=12,sqrt(L) is between 3 and 4.
+Rewritten with 3 rows and 4 columns:
+have
+anic
+eday
+Sample Input 1
+feedthedog    
+Sample Output 1
+fto ehg ee dd
+Explanation 1
+L=10,sqrt(l)  is between 3 and 4.
+Rewritten with 3 rows and 4 columns:
+feed
+thed
+og '''
 
 # Program Code:
 
